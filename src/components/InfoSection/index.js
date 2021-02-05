3c34343519e970896f166701b82c7a1df28029c2
Ayush <ayushadarsh03@gmail.com>
2021-02-05 11:33:03 +0530

    Next page template ready

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,8 +1,46 @@
 import React from "react";
+import { Button } from "../ButtonElement";
+import {
+	InfoContainer,
+	InfoWrapper,
+	InfoRow,
+	Column1,
+	Column2,
+	TextWrapper,
+	TopLine,
+	Heading,
+	Subtitle,
+	BtnWrap,
+	ImgWrap,
+	Img,
+} from "./InfoElements";
 
 const InfoSection = () => {
-	return;
-	<div>Hi</div>;
+	return (
+		<div>
+			<InfoContainer>
+				<InfoWrapper>
+					<InfoRow>
+						<Column1>
+							<TextWrapper>
+								<TopLine>Topline</TopLine>
+								<Heading>Heading</Heading>
+								<Subtitle>Subtitle</Subtitle>
+								<BtnWrap>
+									<Button to="home" />
+								</BtnWrap>
+							</TextWrapper>
+						</Column1>
+						<Column2>
+							<ImgWrap>
+								<Img />
+							</ImgWrap>
+						</Column2>
+					</InfoRow>
+				</InfoWrapper>
+			</InfoContainer>
+		</div>
+	);
 };
 
 export default InfoSection;
